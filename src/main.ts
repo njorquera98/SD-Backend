397bff8bd0d34ca4c64d6a82627ae279baadf61a
3risGreyrat <do42106@gmail.com>
2023-12-08 16:39:26 -0300

    CORS Add

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración de tubos globales (global pipes)
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +13,15 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  // Configuración de CORS
+  app.enableCors({
+    origin: 'http://localhost:9000', // Reemplaza con la URL de tu aplicación Quasar
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Iniciar la aplicación
   await app.listen(3000);
 }
 bootstrap();
